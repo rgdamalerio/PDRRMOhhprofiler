2ab7624e325c2e1f3f7947fcabbcf979abbdf04e
Roel Damalerio <damalerioroel@gmail.com>
2020-08-14 04:16:45 +0800

    openDatabase{

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as FileSystem from "expo-file-system";
 import { Asset } from "expo-asset";
 import * as SQLite from "expo-sqlite";
@@ -6,6 +6,7 @@ import * as SQLite from "expo-sqlite";
 import Respondents from "./app/screens/RespondentScreen";
 
 export default function App() {
+  /*
   async function openDatabaseIShipWithApp() {
     const internalDbName = "hhprofiler.db"; // Call whatever you want
     const sqlDir = FileSystem.documentDirectory + "SQLite/";
@@ -22,6 +23,26 @@ export default function App() {
   const db = SQLite.openDatabase("hhprofiler.db");
 
   console.log(db);
+  */
+  const [db, setDb] = useState(null);
 
+  const openDatabaseIShipWithApp = async () => {
+    const internalDbName = "hhprofiler.db"; // Call whatever you want
+    const sqlDir = FileSystem.documentDirectory + "SQLite/";
+    if (!(await FileSystem.getInfoAsync(sqlDir + internalDbName)).exists) {
+      await FileSystem.makeDirectoryAsync(sqlDir, { intermediates: true });
+      const asset = Asset.fromModule(
+        require("./app/assets/database/hhprofiler.db")
+      );
+      await FileSystem.downloadAsync(asset.uri, sqlDir + internalDbName);
+    }
+    setDb(SQLite.openDatabase("hhprofiler"));
+  };
+
+  /*useEffect(() => {
+    const db = openDatabaseIShipWithApp();
+    //console.log(db);
+  }, []);
+  */
   return <Respondents />;
 }
