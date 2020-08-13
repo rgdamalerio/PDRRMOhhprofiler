10048d20f59830d92c835235fb046319473cdf7d
Roel Damalerio <damalerioroel@gmail.com>
2020-08-13 11:23:55 +0800

    Create Welcome Screen component

---
+++ app/config/colors.js
@@ -0,0 +1,14 @@
+export default {
+  primary: "#2196F3",
+  secondary: "#757575",
+  black: "#212121",
+  white: "#FFFFFF",
+  medium: "#6e6969",
+  light: "#B2EBF2",
+  dark: "#1976D2",
+  danger: "#ff5252",
+  primaryText: "#212121",
+  anccentColor: "#536DFE",
+  dividerColor: "#BDBDBD",
+  darkPrimaryColor: "#0097A7",
+};
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { View, StyleSheet, ImageBackground, Image, Text } from "react-native";
+
+import colors from "../config/colors";
+import Button from "../components/Button";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.logo}
+          source={require("../assets/logo.png")}
+        />
+        <Text style={styles.tagline}>
+          Comprehensive Information System for Disaster Risk Assessment in
+          Agusan del norte
+        </Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button title="Login" />
+        <Button title="Register" color="secondary" />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 180,
+    height: 180,
+    alignSelf: "center",
+    //marginTop: 30,
+    marginBottom: -100,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  tagline: {
+    fontSize: 22,
+    fontWeight: "800",
+    paddingVertical: 100,
+    textAlign: "center",
+    //alignContent: "center",
+    //marginBottom: 50,
+  },
+});
+
+export default WelcomeScreen;
