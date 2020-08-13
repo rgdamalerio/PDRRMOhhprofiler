46395ced0120015b2cb719ad1000eca87dc02c1e
Roel Damalerio <damalerioroel@gmail.com>
2020-08-13 16:03:28 +0800

    Initialize pre-populated sqlite database using expo SQLITE libaray

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
 
-import Register from "./app/screens/RegisterScreen";
+import Respondents from "./app/screens/RespondentScreen";
 
 export default function App() {
-  return <Register />;
+  async function openDatabaseIShipWithApp() {
+    const internalDbName = "hhprofiler.db"; // Call whatever you want
+    const sqlDir = FileSystem.documentDirectory + "SQLite/";
+    if (!(await FileSystem.getInfoAsync(sqlDir + internalDbName)).exists) {
+      await FileSystem.makeDirectoryAsync(sqlDir, { intermediates: true });
+      const asset = Asset.fromModule(
+        require("./app/assets/database/hhprofiler.db")
+      );
+      await FileSystem.downloadAsync(asset.uri, sqlDir + internalDbName);
+    }
+    this.database = SQLite.openDatabase(internalDbName);
+  }
+
+  const db = SQLite.openDatabase("hhprofiler.db");
+
+  console.log(db);
+
+  return <Respondents />;
 }
