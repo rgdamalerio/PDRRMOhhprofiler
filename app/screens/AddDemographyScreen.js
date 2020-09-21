882d66d1783d41e0f1fb89eadb001e0a81ac4515
Roel Damalerio <damalerioroel@gmail.com>
2020-09-21 17:11:07 +0800

    Initial layout for adding demograpy

---
+++ app/screens/AddDemographyScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+function AddDemographyScreen(props) {
+  return (
+    <View style={styles.container}>
+      <Text>Add demography</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default AddDemographyScreen;
